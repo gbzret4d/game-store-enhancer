6afd41913bbd8c188f069184807fc972002f5eee
gbzret4d <skapytek@inbox.lv>
2026-02-15 19:19:12 +0100

    fix: v2.6.10 - implement ignored game border and force window.open on click

--- game_store_enhancer.user.js
+++ game_store_enhancer.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Game Store Enhancer (Dev)
 // @namespace    https://github.com/gbzret4d/game-store-enhancer
-// @version      2.6.9
+// @version      2.6.10
 // @description  Enhances Humble Bundle, Fanatical, DailyIndieGame, and GOG with Steam data (owned/wishlist status, reviews, age rating).
 // @author       gbzret4d
 // @match        https://www.humblebundle.com/*
@@ -247,7 +247,7 @@
     const STEAM_REVIEWS_API = 'https://store.steampowered.com/appreviews/';
     const PROTONDB_API = 'https://protondb.max-p.me/games/';
     const CACHE_TTL = 15 * 60 * 1000; // 15 minutes (v1.25)
-    const CACHE_VERSION = '2.59'; // v2.6.9: Sibling <a> Fix
+    const CACHE_VERSION = '2.60'; // v2.6.10: Finalizing
 
     // Styles
     const css = `
